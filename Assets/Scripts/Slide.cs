c0299dc5a7f92b8edcf5c9623067bb3bca5f0236
Fillipe Ogg <dupuqub@gmail.com>
2019-01-23 21:59:18 -0200

    Improved UI slide function and values.

--- Assets/Scripts/Slide.cs
+++ Assets/Scripts/Slide.cs
@@ -4,25 +4,30 @@ using UnityEngine;
 
 public class Slide : MonoBehaviour
 {
-  public int start;
   public int delay;
+  public int start;
+  public int end;
 
   int currentDelay;
   Vector3 origin;
+  Vector3 future;
 
   void Start()
   {
     currentDelay = delay;
     origin = transform.localPosition;
+    future = new Vector3(end, origin.y, origin.z);
     transform.localPosition = new Vector3(start, origin.y, origin.z);
   }
 
   void FixedUpdate()
   {
+    // Move.
     if(currentDelay > 0) currentDelay --;
-    else transform.localPosition = Vector3.Lerp(transform.localPosition, origin, 0.1f);
+    else transform.localPosition = Vector3.Lerp(transform.localPosition, future, 0.1f);
 
-    if(transform.localPosition.x + 0.1f > origin.x
-    && transform.localPosition.x - 0.1f < origin.x) Destroy(this);
+    // End.
+    if(transform.localPosition.x + 0.1f > end
+    && transform.localPosition.x - 0.1f < end) Destroy(this);
   }
 }
